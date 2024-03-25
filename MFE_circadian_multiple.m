fd868ffeb9134aa837e680cf0ace31b4bff8d55c
wakki-0123 <s2131163ug@s.chibakoudai.jp>
2024-03-25 16:51:04 +0900

    circadian 0325-3 commit

---
+++ MFE_circadian_multiple.m
@@ -0,0 +1,81 @@
+function [e_all, e_IAAFT_all] = MFE_circadian_multiple(data_cell, c, maxiter, m, factor, mf, rn, local, tau)
+% MFE_circadian_multiple: Circadian Rhythm用の複数データを処理する関数
+
+% データの数
+num_data = numel(data_cell);
+
+% 出力用の変数を初期化
+e_all = cell(1, num_data);
+e_IAAFT_all = cell(1, num_data);
+q = 0;
+
+for data_index = 1:num_data
+    % 各データを取得
+    q = q + 1;
+    data = data_cell{data_index};
+    data_l = length(data);
+    
+    % マルチスケールファジーエントロピーとIAAFTを計算
+    [e, e_IAAFT] = MFE_circadian_single(data, c, maxiter, m, factor, mf, rn, local, tau, data_l);
+    
+    % 結果を保存
+    e_all{data_index} = e;
+    e_IAAFT_all{data_index} = e_IAAFT;
+    disp("現在の稼働状況10段階 (全体でどのくらい進んでいるか)")
+    disp(q)
+end
+
+end
+
+function [e1, e_IAAFT] = MFE_circadian_single(data, c, maxiter, m, factor, mf, rn, local, tau, data_l)
+% MFE_circadian_single: 単一のデータに対してマルチスケールファジーエントロピーとIAAFTを計算する関数
+
+% マルチスケールファジーエントロピーの計算
+e1 = fuzzymsentropy(data, m, mf, rn, local, tau, factor);
+
+% IAAFTを実行し、その結果のマルチスケールファジーエントロピーを計算
+e2 = zeros(factor, c);
+[s, ~] = IAAFT(data, c, maxiter);
+
+% サロゲートデータのマルチスケールファジーエントロピーを計算
+for i = 1:c
+    e2(:, i) = fuzzymsentropy(s(:, i), m, mf, rn, local, tau, factor);
+    disp("現在のサロゲートデータに関するファジーエントロピーの様子")
+    disp(i)
+end
+
+% 転置行列
+e2 = e2';
+e_IAAFT = mean(e2);
+
+% グラフの表示
+plot_MFE_graph(e1, e2, data_l);
+
+end
+
+function plot_MFE_graph(e1, e2, data_l)
+% マルチスケールファジーエントロピーのグラフをプロットする関数
+time_length = data_l * 5; % 全部の区間の秒数
+factor = size(e2, 2);
+
+% 時間スケールを計算
+time_s = zeros(1, factor);
+time = zeros(1, factor);
+for i = 1:factor
+    time_s(i) = data_l / i; % 合計サンプルの個数
+    time(i) = time_length / time_s(i); % タイムスケール
+end
+
+% グラフの表示
+figure;
+plot(time, e1, 'r')
+hold on
+errorbar(time, mean(e2), std(e2), 'b');
+legend('ORG', 'IAAFT', 'Location', 'southeast');
+set(gca, 'XScale', 'log');
+hold off
+title('Heart Rate Multiscale Fuzzy Entropy');
+xlabel('Time Scale');
+ylabel('Fuzzy Entropy');
+
+end
---
+++ data_to_cell.m
@@ -0,0 +1,13 @@
+function data_cell = data_to_cell(data1,data2,data3,data4,data5,data6,data7,data8,data9,data10)
+% サイズが 1x10 の空のセル配列を作成
+data_cell = cell(1, 10);
+data_cell{1} = data1;
+data_cell{2} = data2;
+data_cell{3} = data3;
+data_cell{4} = data4;
+data_cell{5} = data5;
+data_cell{6} = data6;
+data_cell{7} = data7;
+data_cell{8} = data8;
+data_cell{9} = data9;
+data_cell{10} = data10;
